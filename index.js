704016e0862c25d9fa5ddb9458f785cf413425aa
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-10-23 14:29:23 +0800

    task completed

--- index.js
+++ index.js
@@ -3,6 +3,25 @@
 const tablist = document.querySelector('[role="tablist"]')
 const tabs = tablist.querySelectorAll('[role="tab"]')
 const panels = document.querySelectorAll('[id^="section"]')
+const overlay = document.querySelector('.overlay');
+const limitedOfferDialog = document.querySelector('.dialog-offer')
+const closeBtn = document.querySelector('.btn__close')
+
+let hours = 24
+let minutes = 0
+let seconds = 0
+
+
+closeBtn.addEventListener('click', () => {
+  overlay.classList.add("hidden")
+  limitedOfferDialog.classList.add("hidden")
+})
+
+
+const closeDialog = () => {
+ overlay.classList.add("hidden")
+ limitedOfferDialog.classList.add("hidden")
+}
 
 const switchTab = (oldTab, newTab) => {
   newTab.focus();
@@ -47,3 +66,50 @@ tabs.forEach((tab, i) => {
   })
 
 })
+
+let totalSeconds = 24 * 60 * 60; // 24 hours in seconds
+
+const updateTime = () => {
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+   // Create arrays of digits
+   const hoursArray = String(hours).padStart(2, '0').split('');
+   const minutesArray = String(minutes).padStart(2, '0').split('');
+   const secondsArray = String(seconds).padStart(2, '0').split('');
+ 
+   // Update the displayed digits
+   updateDigits('hours', hoursArray);
+   updateDigits('minutes', minutesArray);
+   updateDigits('seconds', secondsArray);
+
+    // Decrement the total seconds
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    clearInterval(timerInterval); // Stop the timer when it reaches zero
+    return
+  }
+}
+
+const updateDigits = (id, timeArray) => {
+  const container = document.getElementById(id);
+  const digits = container.querySelectorAll('span');
+
+  digits.forEach((digit, index) => {
+    if (digit.textContent !== timeArray[index]) {
+      container.classList.add('flipping'); // Add flip animation
+
+      setTimeout(() => {
+        digit.textContent = timeArray[index]; // Update the digit after the animation
+        container.classList.remove('flipping'); // Remove flip animation
+      }, 500); // Duration of the flip animatio
+    }
+  });
+}
+
+// Set up the timer when the DOM is fully loaded
+window.addEventListener('DOMContentLoaded', () => {
+  // setTimeout(closeDialog, 5000); // Auto close dialog after 5 seconds
+  timerInterval = setInterval(updateTime, 1000); // Update the time every second
+});n
